0d7281ea944a4a8c57e53e24e8a0ac026bf47893
lvdh0002 <vanderheijdenlot@gmail.com>
2025-11-27 16:24:34 +0100

    Update app-code.py

--- app-code.py
+++ app-code.py
@@ -1,6 +1,3 @@
-# -------------------------
-# PART 1: IMPORTS AND SESSION STATE
-# -------------------------
 import streamlit as st
 import pandas as pd
 import math
@@ -15,23 +12,18 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
 # -------------------------
-# Session state defaults
+# Session state: prijs/kwaliteit sync
 # -------------------------
 if "prijs_pct" not in st.session_state:
     st.session_state.prijs_pct = 40
 if "kwaliteit_pct" not in st.session_state:
     st.session_state.kwaliteit_pct = 60
-if "criteria" not in st.session_state:
-    st.session_state.criteria = ["Flexibiliteit", "Dienstverlening", "Duurzaamheid"]
-if "score_scale" not in st.session_state:
-    st.session_state.score_scale = [0, 20, 40, 60, 80, 100]
-if "num_competitors" not in st.session_state:
-    st.session_state.num_competitors = 3
-if "price_weight" not in st.session_state:
-    st.session_state.price_weight = 40
-if "margin_pct" not in st.session_state:
-    st.session_state.margin_pct = 10.0
 
+def update_prijs():
+    st.session_state.kwaliteit_pct = 100 - st.session_state.prijs_pct
+
+def update_kwaliteit():
+    st.session_state.prijs_pct = 100 - st.session_state.kwaliteit_pct
 
 
 # -------------------------
